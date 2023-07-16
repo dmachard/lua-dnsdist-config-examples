fa85d1fdfcd695f41ed4b97ccd423cd5a3740ad1
Denis MACHARD <5562930+dmachard@users.noreply.github.com>
2023-07-16 12:10:41 +0200

    Update security_blocking_dnstunneling.lua

--- security_blocking_dnstunneling.lua
+++ security_blocking_dnstunneling.lua
@@ -1,35 +1,46 @@
--- Generic rules to block malicious dns traffic with regex
+-- Generic rules to block malicious dns traffic like DNS tunneling
 
--- remote dnstap logger
+-- remote security events logger based on dnstap
 security_dnstap_logger = newFrameStreamTcpLogger("10.0.0.100:6000")
 
--- white list to ignore some trusted domains
-malicious_white_list = newSuffixMatchNode()
-malicious_white_list:add(newDNSName("google.fr"))
+-- trusted domains to exclude
+trusted_domains = newSuffixMatchNode()
+trusted_domains:add(newDNSName("google.fr."))
+
+-- blacklist ip during 60s
+blacklisted_ips = TimedIPSetRule()
+local function blacklistIP(dq)
+   blacklisted_ips:add(dq.remoteaddr, 60)
+   return DNSAction.Refused
+end
+addAction(blacklisted_ips:slice(), RCodeAction(DNSRCode.REFUSED))
 
 -- match uncommon qtype like NULL (10), PRIVATE (65399) - works fine to block iodine
-addAction(AndRule({OrRule({QTypeRule(10), QTypeRule(65399)}), NotRule(SuffixMatchNodeRule(malicious_white_list, true)) }), SetTagAction('block_malicious', ''))
+addAction(AndRule({OrRule({QTypeRule(10), QTypeRule(65399)}), NotRule(SuffixMatchNodeRule(trusted_domains, true)) }), SetTagAction('malicious_qtypes', 'matched'))
 
 -- match long qname - 2 labels with a minimum of 50 bytes each - works fine to block tools like iodine, dnscat2, dns2tcp...
 -- to avoid false-positive. The regex has been tested on the top 1 million list exposed by Cisco Umbrella http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip
-addAction(AndRule({RegexRule('([^.]{50,}\\.){2,}'), NotRule(SuffixMatchNodeRule(malicious_white_list, true)) }), SetTagAction('block_malicious', ''))
+addAction(AndRule({RegexRule('([^.]{50,}\\.){2,}'), NotRule(SuffixMatchNodeRule(trusted_domains, true)) }), SetTagAction('malicious_longqnames', 'matched'))
+
+-- rate limiting on TXT, CNAME and MX
+addAction(AndRule({ MaxQPSIPRule(5, 32, 64, 5), OrRule({QTypeRule(DNSQType.TXT), QTypeRule(DNSQType.CNAME), QTypeRule(DNSQType.MX)}), NotRule(SuffixMatchNodeRule(trusted_domains, true)) }), SetTagAction('malicious_ratelimiting', 'matched'))
 
 -- logging malicious queries on remote dnstap logger
-addAction(TagRule('block_malicious'), DnstapLogAction("malicious_blocked", security_dnstap_logger))
+addAction(TagRule('malicious_qtypes'), DnstapLogAction("event_deniedqtypes_detected", security_dnstap_logger))
+addAction(TagRule('malicious_longqnames'), DnstapLogAction("event_longqnames_detected", security_dnstap_logger))
+addAction(TagRule('malicious_ratelimiting'), DnstapLogAction("event_ratelimiting_detected", security_dnstap_logger))
 
--- finally refused malicious queries
-addAction(TagRule('block_malicious'), RCodeAction(DNSRCode.REFUSED))
+-- finally refuses and blacklist client ip during limited time
+addAction(TagRule('malicious_qtypes'), LuaAction(blacklistIP))
+addAction(TagRule('malicious_longqnames'), LuaAction(blacklistIP))
+addAction(TagRule('malicious_ratelimiting'), LuaAction(blacklistIP))
 
 -- Update the dynamic blocks with refused reply by default
 setDynBlocksAction(DNSAction.Refused)
 
 -- Rate exceeded detection with automatic ip blacklisting during 60s
---  * max 5req/s during 5s for TXT, CNAME and MX
 --  * max bw to 1000bytes/s during 5s
 local dbr = dynBlockRulesGroup()
-dbr:setQTypeRate(DNSQType.TXT, 5, 5, "Exceeded TXT rate", 60)
-dbr:setQTypeRate(DNSQType.CNAME, 5, 5, "Exceeded CNAME rate", 60)
-dbr:setQTypeRate(DNSQType.MX, 5, 5, "Exceeded MX rate", 60)
 dbr:setResponseByteRate(1000, 5, "Exceeded resp BW rate", 60)
 
 -- check dynamic rule every second
