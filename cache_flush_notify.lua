29868fc8c233decedc13ee760fb4fc539bc26ba6
Denis MACHARD <5562930+dmachard@users.noreply.github.com>
2023-07-16 16:50:06 +0200

    Update cache_flush_notify.lua

--- cache_flush_notify.lua
+++ cache_flush_notify.lua
@@ -1,1 +1,11 @@
 
+-- return CNAME with flushed name
+local function onNotifyFlush(dq)
+        getPool("default"):getCache():expungeByName(dq.qname, DNSQType.ANY, true)
+        return DNSAction.Spoof, "flushed"
+end
+
+-- flush domain entry on incoming notify
+-- dig @127.0.0.1 google.fr +opcode=notify
+addAction(OpcodeRule(DNSOpcode.Notify), LuaAction(onNotifyFlush))
+
