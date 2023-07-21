8398fc75ef52ea712e73ed601ad8d79a366417b1
Denis MACHARD <5562930+dmachard@users.noreply.github.com>
2023-07-21 11:12:07 +0200

    Update security_blackhole_domains.lua

--- security_blackhole_domains.lua
+++ security_blackhole_domains.lua
@@ -1,11 +1,15 @@
 -- example to spoof some domains to localhost for A and AAAA queries
 
 -- blackhole list from external files
--- example of content
--- cat /etc/dnsdist/blackhost_list.txt
--- baddomain.fr.
 blackhole_domains = newSuffixMatchNode()
-for l in io.lines("/etc/dnsdist/blackhost_list.txt") do blackhole_domains:add(newDNSName(l)) end
+for l in io.lines("/etc/dnsdist/blocklist.txt") do
+  if l ~= "" then
+    -- ignore commented lines
+    if l:find("^#") == nil then
+    	blackhole_list:add(newDNSName(l))
+    end
+  end
+end
 
 -- match blackhole domains and spoof response to localhost
 addAction(SuffixMatchNodeRule(blackhole_domains, true), SpoofAction({"127.0.0.1", "[::1]"}, {ttl=3600}), {name="rule_blackholes"})
